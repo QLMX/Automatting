dc50140d28c64db36179d6384bb63c6474643b71
QLMX <wenruichn@gmail.com>
2018-07-11 23:40:26 +0800

    add more insight
    Merge remote-tracking branch 'origin/master'

---
+++ segmantic/config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time  : 18-7-11 下午10:32
+# @Autor : QLMX
+# @Email : wenruichn@gmail.com
+
+#the ori data path
+BASE_DIR = '/home/jrs1049/Files/QLMX/graduate/'
+PORTRAIT_IMG_PATH = BASE_DIR + 'data/portrait/image/'
+PORTRAIT_ANN_PATH = BASE_DIR + 'data/portrait/annotation/'
+PORTRAIT_IMG_PATH = BASE_DIR + 'data/portrait/image/'
+PORTRAIT_ANN_PATH = BASE_DIR + 'data/portrait/annotation/'
+PORTRAIT_IMG_PATH = BASE_DIR + 'data/portrait/image/'
+PORTRAIT_ANN_PATH = BASE_DIR + 'data/portrait/annotation/'
+PORTRAIT_IMG_PATH = BASE_DIR + 'data/portrait/image/'
+PORTRAIT_ANN_PATH = BASE_DIR + 'data/portrait/annotation/'
+
+#the tranisition data path
+TFRECORD_PATH = BASE_DIR + 'tfrecord/'
+
--- segmantic/preprocess/convert_tfrecord.py
+++ segmantic/preprocess/convert_tfrecord.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 18-7-11 下午10:07
+# @Time  : 18-7-11 下午10:32
+# @Autor : QLMX
+# @Email : wenruichn@gmail.com
+import tensorflow as tf
+
+
+import sys
+sys.path
